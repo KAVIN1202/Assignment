71f73194d7f689d0b8d4e322d56da888ccd79d37
KAVIN1202 <91768828+KAVIN1202@users.noreply.github.com>
2021-10-02 10:00:29 +0530

    Day 1 Assignment Submitted

---
+++ Assignment1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Assignment1
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("My name is Kavin v");
+        System.out.println("I'm Computer science stdent");
+        System.out.println("I'm pursuing my degree in Gobi Arts & Science College");
+        System.out.println("This is my first Additional course");
+        System.out.println("My hobbie is to riding bikes");
+        System.out.println("My AIM is to be an IT profession");
+    }
+}
